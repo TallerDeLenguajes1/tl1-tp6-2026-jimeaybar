ad694971e10451d37d1745c45abe00a3e7f467aa
jimeaybar <jimeaybar1999@gmail.com>
2026-05-19 13:48:43 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -8,3 +8,22 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+int num, invertido=0;
+string? entrada;
+Console.WriteLine("Ingrese un numero:");
+entrada=Console.ReadLine();
+
+while(!int.TryParse(entrada, out num)){
+    Console.WriteLine("Error no es un numero");
+    Console.WriteLine("Ingrese un número nuevamente:");
+    entrada=Console.ReadLine();
+}
+
+while(num > 0){
+    int ultimo= num%10;
+    invertido = (invertido * 10) + ultimo;
+    num = num /10;
+}
+
+Console.WriteLine("Numero invertido:"+invertido);
